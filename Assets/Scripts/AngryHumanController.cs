99497ad3f50f105e58c98169c355be410afcc7aa
Nazarii Klymok <37501778+knazariy@users.noreply.github.com>
2021-10-30 16:56:56 +0300

    Revert "Add killing skelleton logic"

--- Assets/Scripts/AngryHumanController.cs
+++ Assets/Scripts/AngryHumanController.cs
@@ -3,43 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class AngryHumanController : MonoBehaviour
 {
-    private const string GameOverSceneName = "GameOver";
-    private const string PlayerTagName = "Player";
-    private const string BulletTagName = "Bullet";
 
     [SerializeField] private Transform _movePoint;
     [SerializeField] private float _moveSpeed = 10f;
 
+    // Start is called before the first frame update
     private void Start()
     {
         _movePoint.parent = null;
     }
 
+    // Update is called once per frame
     private void Update()
     {
-        if (Vector3.Distance(transform.position, _movePoint.position) == 0f)
-        {
+        if (Vector3.Distance(transform.position, _movePoint.position) == 0f) {
             RespawnAndSetFinalMovePoint();
         }
         transform.position = Vector3.MoveTowards(transform.position, _movePoint.position, _moveSpeed * Time.deltaTime);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag(PlayerTagName))
-        {
-            SceneManager.LoadScene(GameOverSceneName);
-        }
 
-        if (other.gameObject.CompareTag(BulletTagName))
-        {
-            RespawnAndSetFinalMovePoint();
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) {
+            SceneManager.LoadScene("GameOver");
         }
     }
 
-    private void RespawnAndSetFinalMovePoint() 
-    {
-        bool topToBottom = Random.Range(0, 2) == 0;
+    private void RespawnAndSetFinalMovePoint() {
+        bool topToBottom = Random.Range(0, 2) == 0 ? true : false;
         if (topToBottom)
         {
             transform.position = new Vector3(Random.Range(0, 32), 18, transform.position.z);
