dc8669ef981b38ca7496cc4ee9b707e294a79ade
Nazarii Klymok <37501778+knazariy@users.noreply.github.com>
2021-10-31 02:01:09 +0300

    added hearts (#7)
    * added hearts
    
    * comment fixes
    
    * comment fixes #2
    
    Co-authored-by: Nazarii Klymok <nazarii.klymok@botscrew.com>

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField] private int _health;
+    [SerializeField] private int _numOfHearts;
+
+    [SerializeField] private Image[] hearts;
+    [SerializeField] private Sprite fullHeart;
+    [SerializeField] private Sprite emptyHeart;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateHeartStatusBar();
+    }
+
+    private void UpdateHeartStatusBar()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < _health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < _numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
