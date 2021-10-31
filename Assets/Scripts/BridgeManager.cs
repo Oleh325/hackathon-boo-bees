b8eb2efec3ea6cb9bb60cb1dbbf0230547044e4d
fuvaji <90569152+fuvaji@users.noreply.github.com>
2021-10-31 11:30:57 +0200

    Hbb 41 bridge logic (#23)
    * Added basic bridge logic
    
    * bridges are walkable
    
    * Bridges logic done

---
+++ Assets/Scripts/BridgeManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _bridgeParent;
+    [SerializeField] private Timer _timer;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _timer.OnDayNightTransition += SetBridgesActive;
+    }
+
+    private void OnDestroy()
+    {
+        _timer.OnDayNightTransition -= SetBridgesActive;
+    }
+
+    // Update is called once per frame
+    private void SetBridgesActive(bool isDay)
+    {
+        foreach (Transform child in _bridgeParent.transform)
+        {
+            Debug.Log("Setting bridges to" + isDay);
+            child.gameObject.SetActive(isDay);
+        }
+    }
+}
