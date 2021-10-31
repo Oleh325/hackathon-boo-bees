d0f605fc2114fbd9a9dafdb3b075449419c6d1a5
cupoftea4 <90093980+cupoftea4@users.noreply.github.com>
2021-10-31 15:13:23 +0200

    cat

---
+++ Assets/Scripts/CatGuide.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class CatGuide : MonoBehaviour
+{
+    [SerializeField] private GameObject cat;
+    [SerializeField] private GameObject guideText;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            cat.SetActive(false);
+            guideText.SetActive(false);
+        }
+    }
+}
