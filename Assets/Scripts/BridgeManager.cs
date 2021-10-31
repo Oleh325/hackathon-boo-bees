57f357c4bca1e22da2db129a7e9ae2048a5e4375
fuvaji <vikusya.derpak@gmail.com>
2021-10-31 09:31:31 +0200

    Added basic bridge logic

---
+++ Assets/Scripts/BridgeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _bridgeParent;
+    [SerializeField] private Timer _timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool isDay = _timer.isDay;
+        foreach(Transform child in _bridgeParent.transform)
+        {
+            child.gameObject.SetActive(isDay);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
