90ae81b43f781f6c363d463a1197518c49e37fc0
cupoftea4 <90093980+cupoftea4@users.noreply.github.com>
2021-10-31 09:10:45 +0200

    Skeleton animations and death logic

--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -3,15 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class Skeleton : MonoBehaviour
 {
+    private enum SkeletonState
+    {
+        IsMovingDown,
+        IsMovingUp,
+        IsMovingLeft,
+        IsMovingRight,
+        IsRuined,
+        None
+    }
     
+    private const string IslandTagName = "Island";
     private const string GameOverSceneName = "GameOver";
     private const string PlayerTagName = "Player";
     private const string BulletTagName = "Bullet";
 
     [SerializeField] private Transform _targetTransform;
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private Animator _animator;
     private Rigidbody2D _rigidbody2D;
     private Vector2 _movement;
+    private bool _isDead = false;
 
     private void Awake()
     {
@@ -24,15 +36,36 @@ public class Skeleton : MonoBehaviour
         Vector3 direction = _targetTransform.position - transform.position;
         direction.Normalize();
         _movement = direction;
+        SetDirectionAnimation(direction);
     }
 
-    private void FixedUpdate()
+    private void SetDirectionAnimation(Vector3 direction)
     {
+        if (_isDead)
+        {
+            return;
+        }
         MoveCharacter(_movement);
+        if (Mathf.Abs(direction.y / direction.x) <= 1f)
+        {
+            _animator.SetTrigger(direction.x >= 0
+                ? Animator.StringToHash(SkeletonState.IsMovingRight.ToString())
+                : Animator.StringToHash(SkeletonState.IsMovingLeft.ToString()));
+        }
+        else
+        {
+            _animator.SetTrigger(direction.y >= 0
+                ? Animator.StringToHash(SkeletonState.IsMovingUp.ToString())
+                : Animator.StringToHash(SkeletonState.IsMovingDown.ToString()));
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (_isDead)
+        {
+            return;
+        }
         if (other.gameObject.CompareTag(PlayerTagName))
         {
             SceneManager.LoadScene(GameOverSceneName);
@@ -40,9 +73,32 @@ public class Skeleton : MonoBehaviour
 
         if (other.gameObject.CompareTag(BulletTagName))
         {
-            Destroy(gameObject);
+            
+            _isDead = true;
+            ResetAllTriggers();
+            _animator.SetTrigger(Animator.StringToHash(SkeletonState.IsRuined.ToString()));
+        }
+
+        if (other.gameObject.CompareTag(IslandTagName))
+        {
+            //StartCoroutine(DieWithDelay());
+            _isDead = true;
+            ResetAllTriggers();
+            _animator.SetTrigger(Animator.StringToHash(SkeletonState.IsRuined.ToString()));
         }
     }
+
+    private void ResetAllTriggers()
+    {
+        foreach (var param in _animator.parameters)
+        {
+            if (param.type == AnimatorControllerParameterType.Trigger)
+            {
+                _animator.ResetTrigger(param.name);
+            }
+        }
+    }
+    
     private void MoveCharacter(Vector2 direction)
     {
         _rigidbody2D.MovePosition((Vector2) transform.position + (direction * _moveSpeed * Time.deltaTime));
